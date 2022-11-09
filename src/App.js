cf5ffc0d66e3617199715da0de0d01271b8c6711
Norbert Csipak <norbertcipak@me.com>
2022-11-09 12:27:50 +0000

    component AddAppointment added to the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BiArchive } from "react-icons/bi";
 import Search from "./components/Search";
+import AddAppointment from "./components/AddAppointment";
 
 function App() {
   return (
     <div className="App container mx-auto mt-3">
       <h1 className="flex font-bold p-3">
       <BiArchive className="text-2xl text-red-300 inline-block align-top" />Your Appointments</h1>
+      <AddAppointment />
       <Search />
     </div>
   );
