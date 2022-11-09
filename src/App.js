6ea2f12dfad9b65754ba3d7f593e2941689614f5
Norbert Csipak <norbertcipak@me.com>
2022-11-09 15:37:40 +0000

    filtering the original list of appointments and matching the words typed in search box

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ import ComponentInfo from "./components/AppointmentInfo";
 function App() {
 
   let [appointmentList, setAppointmentList] = useState([]);
+  let [query, setQuery] = useState('');
+
+  const filteredAppoointments = appointmentList.filter(
+    item => {
+      return (
+        item.petName.toLowerCase().includes(query.toLowerCase()) ||
+        item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+        item.aptNotes.toLowerCase().includes(query.toLowerCase())
+        )
+    }
+  )
 
   const fetchData = useCallback(() => {
     fetch('./data.json')
@@ -23,12 +34,17 @@ function App() {
       <h1 className="flex font-bold p-3 mb-3">
       <BiCalendar className="text-2xl text-red-300 inline-block align-top" />Your Appointments</h1>
       <AddAppointment />
-      <Search />
+      <Search query={query}
+        onQueryChange={myQuery => {setQuery(myQuery)}} />
 
       <ul className="divide-y divide-gray-200">
-        {appointmentList.map(appointment => (
+        {filteredAppoointments.map(appointment => (
             <ComponentInfo key={appointment.id}
               appointment={appointment}
+              onDeleteAppointment={
+                appointmentId => setAppointmentList(
+                  appointmentList.filter(appointment = appointment.id === appointmentId))
+              }
             />
           ))
         }
--- src/components/Search.js
+++ src/components/Search.js
@@ -30,7 +30,7 @@ const DropDown = ({ toggle }) => {
   )
 }
 
-const Search = () => {
+const Search = ({query, onQueryChange }) => {
   let [toggleSort, setToggleSort] = useState(false);
   return (
     <div className="py-5">
@@ -41,7 +41,9 @@ const Search = () => {
         <label htmlFor="query" className="sr-only" />
       </div>
 
-      <input type="text" name="query" id="query" value=""
+      <input
+        onChange={(event) =>{onQueryChange(event.target.value)}}
+        type="text" name="query" id="query" value={query}
         className="pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300" placeholder="Search" />
 
       <div className="absolute inset-y-0 right-0 flex items-center">
